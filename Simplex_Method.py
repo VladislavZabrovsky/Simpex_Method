c22d9a589882cdf5edd6168d2d9f3f2dde8c049e
VladislavZabrovsky <124517671+VladislavZabrovsky@users.noreply.github.com>
2023-12-06 11:47:07 +0200

    Update Simplex_Method.py
    Decided to separate methods for simplex method algorythm from main code. You can use the calculator in calculator.py

--- Simplex_Method.py
+++ Simplex_Method.py
@@ -173,31 +173,3 @@ def check_rhs_value(user_input):
         return True
     return False
 
-def main():
-    num_variables = int(input("Enter the number of variables: "))
-    if num_variables <= 1:
-        raise ValueError("Number of variables should be at least 2")
-    num_constraints = int(input("Enter the number of constraints: "))
-    if num_constraints <= 0:
-        raise ValueError("Invalid quantity of constraints")
-
-    obj_coefficients, constraint_coefficients, rhs_values = create_simplex_input(num_variables, num_constraints)
-    print_linear_programming_problem(obj_coefficients, constraint_coefficients, rhs_values)
-
-    table = create_simplex_table(obj_coefficients, constraint_coefficients, rhs_values)
-    print_simplex_table(table)
-
-    simplex_method(obj_coefficients, constraint_coefficients, rhs_values)
-
-
-if __name__ == "__main__":
-    try:
-        print("""
-        
-SIMPLEX METHOD CALCULATOR
-Author: Vladislav Zabrovsky
-        
-        """)
-        main()
-    except Exception as e:
-        print(f"Wow,the problem appeared.Explanation: {e}")
